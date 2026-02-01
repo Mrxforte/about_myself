fa8fe6215b4a4687bfb7a425d5115cb6d8a6f92d
MrxForte <urazimbetovazamat6@gmail.com>
2026-01-31 18:12:08 -0800

    added custom appbar

--- lib/views/home/web/home_screen_web.dart
+++ lib/views/home/web/home_screen_web.dart
@@ -5,6 +5,37 @@ class HomeScreenWeb extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 32),
+        width: width,
+        height: height,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // custom appbar
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 32),
+                width: width,
+                height: 60,
+                child: Row(
+                  children: [
+                    TextButton(onPressed: () {}, child: Text("Home")),
+                    TextButton(onPressed: () {}, child: Text("Case Studies")),
+                    TextButton(onPressed: () {}, child: Text("Testimonials")),
+                    TextButton(onPressed: () {}, child: Text("Recent Works")),
+                    TextButton(onPressed: () {}, child: Text("Get In Touch")),
+                  ],
+                ),
+              ),
+              // row about user  and avatar
+              // hlist trusted companies
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
